c4bd032650f10747c788b7e49b177f4201109c3f
kartikey Shahi <kartikeyshahi@hotmail.com>
2025-01-10 00:28:33 +0530

    minor bug fixes

--- visualisation/bar_chart.py
+++ visualisation/bar_chart.py
@@ -2,12 +2,16 @@ from typing import List
 
 import plotext as plt
 
+from todo.user_commands.helpers import panic
+
 from .helper_functions import get_category_with_number_of_sizes
 
 cat_names, total_tasks = get_category_with_number_of_sizes()
 
 
 def plot_bar(xticks: List[str] = cat_names, yticks: List[int] = total_tasks) -> None:
+    if len(yticks) <= 1:
+        panic("Lack of data can't peform plotting")
     plt.theme("dark")
     plt.bar(xticks, yticks)
     plt.plot_size(plt.terminal_width() / 2, plt.terminal_height() / 3)
--- visualisation/pie_chart.py
+++ visualisation/pie_chart.py
@@ -2,12 +2,16 @@ from typing import List
 
 import termcharts
 
+from todo.user_commands.helpers import panic
+
 from .helper_functions import get_category_with_number_of_sizes
 
 names, tasks = get_category_with_number_of_sizes()
 
 
 def plot_pie(xticks: List[str] = names, yticks: List[int] = tasks):
+    if len(xticks) <= 1:
+        panic("Lack of data can't do plotting")
     chart = termcharts.pie(
         {name: count for name, count in zip(xticks, yticks)}, title="categories"
     )
