cc5f0d4506b0c7f691831f9f1760b27e970e5fcf
kartikey Shahi <kartikeyshahi@hotmail.com>
2025-01-11 14:29:38 +0530

    made db file to be absolute to wherever the command is begin runned

--- database/db.py
+++ database/db.py
@@ -1,9 +1,18 @@
+from pathlib import Path
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+# this will makesure that always the command is run it will use the database in the below path
+
+DB_PARENT_PATH = Path("~/.toshu").expanduser()
+DB_PARENT_PATH.mkdir(exist_ok=True)
+
+DB_PATH = DB_PARENT_PATH.resolve() / "todo.db"
+
 from .models import Base
 
-engine = create_engine("sqlite:///todo.db")
+engine = create_engine(f"sqlite:///{DB_PATH}")
 
 SessionLocal = sessionmaker(bind=engine)
 
