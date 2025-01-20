04904e9cb304b968c7944b06059686a7a03afb72
kartikey Shahi <kartikeyshahi@hotmail.com>
2025-01-20 16:39:41 +0530

    can't fetch version from pyproject.toml as it is not included in the package build

--- toshu/version.py
+++ toshu/version.py
@@ -1,16 +1,9 @@
-from pathlib import Path
-
-import toml
 import typer
 from rich import print
 
 app = typer.Typer()
 
-pyproject_path = Path(__file__).resolve().parent.parent / "pyproject.toml"
-
-with pyproject_path.open() as project_config_file:
-    config = toml.load(project_config_file)
-    VERSION = config["project"]["version"]
+VERSION = "0.03.1"
 
 
 @app.command(short_help="Show version, and exit")
