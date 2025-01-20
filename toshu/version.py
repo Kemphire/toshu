71b174898fa9e3e5c2d05013da7b295d1027eb06
kartikey Shahi <kartikeyshahi@hotmail.com>
2025-01-20 16:21:31 +0530

    added automatic version fetching from pyproject.toml file

--- toshu/version.py
+++ toshu/version.py
@@ -1,9 +1,16 @@
+from pathlib import Path
+
+import toml
 import typer
 from rich import print
 
 app = typer.Typer()
 
-VERSION = 0.02
+pyproject_path = Path(__file__).resolve().parent.parent / "pyproject.toml"
+
+with pyproject_path.open() as project_config_file:
+    config = toml.load(project_config_file)
+    VERSION = config["project"]["version"]
 
 
 @app.command(short_help="Show version, and exit")
